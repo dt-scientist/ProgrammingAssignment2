33be277ff3ff8854099fae33c301877daa724636
dt-scientist <gmoscholeas@hotmail.com>
2014-07-17 22:02:34 +0100

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCacheMatrix() creates a matrix with a given dimension and element value 
+# when you supply the number of rows and columns, and an element value for the new matrix. 
+# Make sure you pass an invertible square matrix
+# This function contains 4 accessors: get, set, setInvMtx, getInvMtx
+#  set() set the square matrix
+#  get() returns the square matrix
+# setInvMtx() computes the inverse of a matrix
+# getInvMtx() returns the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # invMtx holds the inverse matrix
+  invMtx <- NULL
+  set <- function(y) {
+    x <<- y
+    invMtx <<- NULL
+  }
+  get <- function() x
+  
+  # use the << operator to cache the inverse of a matrix so that when we need 
+  # it again, it can be looked up in the cache rather than recomputed
+  setInvMtx <- function(solve) invMtx <<- solve
+  
+  getInvMtx <- function() invMtx
+  list(set = set, get = get,
+       setInvMtx = setInvMtx,
+       getInvMtx = getInvMtx)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve() computes the inverse of the "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already 
+# been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # invMtx holds the inverse matrix
+  invMtx <- x$getInvMtx()
+  
+  # If the inverse has already been calculated retrieve the inverse from the cache
+  if(!is.null(invMtx)) {
+    message("getting cached data")
+    return(invMtx)
+  }
+  
+  # If the inverse has NOT already been calculated, then the
+  # inverse can be computed by calling setInvMtx()
+  data <- x$get()
+  invMtx <- solve(data, ...)
+  x$setInvMtx(invMtx)
+  invMtx
+  
 }
+
+# Test script
+# mtx<-makeCacheMatrix(matrix(1:4,2,2))
+# mtx$get()
+# cacheSolve(mtx)  
+# cacheSolve(mtx)  
+
+# above example calls cacheSolve() twice
+# the first call computes the inverse
+# the second call retrieves the inverse from the cache
+
